341bef86cf3236010c24d9cefee2a7ad14bbfcb0
CougarChan <gohan0136@gmail.com>
2022-06-09 11:31:44 -0400

    update

---
+++ routes/Html-route.js
@@ -0,0 +1,15 @@
+const router =require ('express').router();
+const path   =require ('path');
+
+
+router.get('/notes',(req, res) => {
+res.sendFile(path.join(__dirname, "../../public/notes.html"));
+
+});
+
+router.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+    
+    });
+
+module.exports =router;
---
+++ routes/api/API-route.js
@@ -0,0 +1,16 @@
+const { json } = require('express/lib/response');
+const fs = require('fs')
+const path = require('path')
+
+function createNewNote(body, noteArr){
+    const note = body;
+    noteArr.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({noteArr}, null, 2)        
+    );
+    return note    
+}
+module.exports = {
+    createNewNote,
+}
